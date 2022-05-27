659a1ecdaa45fa66b96e47247c6bc8d6c0ec629a
iqbalgithub1998 <iqbalkhanak45@gmail.com>
2022-05-28 03:01:05 +0530

    edit index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import 'flowbite';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
